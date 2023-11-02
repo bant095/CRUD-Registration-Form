99301e9efbea9f19c91843cb4a2d59c15c231643
Abolaji Ayodeji <abolajiayodeji93@gmail.com>
2023-11-02 08:33:06 +0100

    I updated the registration form

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Utility function to generate a unique ID
 function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
@@ -6,8 +7,11 @@ function uuid() {
   });
 }
 
+//////////////////////////////////
+// Create - Save User Details Function
 const DB_NAME = 'user_db';
 
+// Getting user details from form inputs
 function saveUserDetails() {
   const username = document.querySelector('#username').value;
   const fullName = document.querySelector('#full-name').value;
@@ -18,8 +22,9 @@ function saveUserDetails() {
   const phoneNumber = document.querySelector('#phone-number').value;
   const gender = document.querySelector('#gender').value;
 
+  // Creating an object for the user
   const userDetails = {
-    id: uuid(),
+    id: uuid(), // Generate a unique ID
     username: username,
     fullName: fullName,
     password: password,
@@ -31,8 +36,13 @@ function saveUserDetails() {
     created_at: Date.now(),
   };
 
+  // Retrieving the user details from local storage or initializing an empty array
   const user_db = JSON.parse(localStorage.getItem(DB_NAME)) || [];
+
+  // Adding the new user details to the array
   const new_user_db = [...user_db, userDetails];
+
+  // Storing the updated user details array back in local storage
   localStorage.setItem(DB_NAME, JSON.stringify(new_user_db));
 
   fetchUserDetails();
@@ -40,19 +50,26 @@ function saveUserDetails() {
   document.querySelector('#user-details-form').reset();
 }
 
+////////////////////////
+// Read Function
 function fetchUserDetails() {
   const userDetailsContainer = document.querySelector(
     '#user-details-container'
   );
+  // Getting user details from local storage
   const user_db = JSON.parse(localStorage.getItem(DB_NAME)) || [];
   const noUser = user_db.length === 0;
   if (noUser) {
-    userDetailsContainer.innerHTML = `<p class="text-center text-slate-500">No user details found.</p>`;
+    userDetailsContainer.innerHTML = `<p class="text-center text-slate-500">No user details found, Enter your Details</p>`;
     return;
   }
 
-  const users = user_db.map((user) => {
-    return `<div class="border rounded-lg p-3 my-2">
+  const users = user_db
+    .sort((a, b) =>
+      a.created_at < b.created_at ? 1 : a.created_at > b.created_at ? -1 : 0
+    )
+    .map((user) => {
+      return `<div class="border rounded-lg p-3 my-2">
     <p><strong>Username:</strong> ${user.username}</p>
     <p><strong>Full Name:</strong> ${user.fullName}</p>
     <p><strong>Email:</strong> ${user.email}</p>
@@ -63,10 +80,12 @@ function fetchUserDetails() {
     <button onclick="editUser('${user.id}')" class="bg-blue-500 text-white px-3 py-1 rounded-md mt-2">Edit</button>
     <button onclick="deleteUser('${user.id}')" class="bg-red-500 text-white px-3 py-1 rounded-md mt-2">Delete</button>
   </div>`;
-  });
+    });
   userDetailsContainer.innerHTML = users.join('');
 }
 
+// ///////////////////////////////////
+// Update/Edit Function
 function editUser(id) {
   const user_db = JSON.parse(localStorage.getItem(DB_NAME)) || [];
   const userToEdit = user_db.find((user) => user.id === id);
@@ -75,6 +94,7 @@ function editUser(id) {
     return;
   }
 
+  // Populating the form with the user details for editing
   document.querySelector('#username').value = userToEdit.username;
   document.querySelector('#full-name').value = userToEdit.fullName;
   document.querySelector('#password').value = userToEdit.password;
@@ -84,15 +104,19 @@ function editUser(id) {
   document.querySelector('#phone-number').value = userToEdit.phoneNumber;
   document.querySelector('#gender').value = userToEdit.gender;
 
+  //To hide and update btn
   const updateUserBtn = document.querySelector('#Update_Users_btn');
   updateUserBtn.classList.remove('hidden');
   updateUserBtn.setAttribute('user_id_to_update', id);
 }
 
+// Update/Edit Function to Save Updated User Details
+
 function updateUser() {
   const user_id_to_update = document
     .querySelector('#Update_Users_btn')
     .getAttribute('user_id_to_update');
+  // Get the user details from the form inputs
   const username = document.querySelector('#username').value;
   const fullName = document.querySelector('#full-name').value;
   const password = document.querySelector('#password').value;
@@ -102,7 +126,9 @@ function updateUser() {
   const phoneNumber = document.querySelector('#phone-number').value;
   const gender = document.querySelector('#gender').value;
 
+  // Retrieving the user details from local storage or initialize an empty array
   const user_db = JSON.parse(localStorage.getItem(DB_NAME)) || [];
+  // Updating the specific user's details in the array
   const updated_user_db = user_db.map((user) => {
     if (user.id === user_id_to_update) {
       return {
@@ -131,23 +157,29 @@ function updateUser() {
   updateUserBtn.classList.add('hidden');
 }
 
+// ///////////////////
+// Delete Function to Delete a User
 function deleteUser(id) {
   Swal.fire({
-    title: 'Delete User',
-    text: 'Are you sure you want to delete this user?',
+    title: 'Delete Form',
+    text: 'Are you sure you want to delete this Form Details?',
     icon: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#d33',
     cancelButtonColor: '#3085d6',
     confirmButtonText: 'Yes, delete it!',
+    //to confirm the cancel btn
   }).then((result) => {
     if (result.isConfirmed) {
       const user_db = JSON.parse(localStorage.getItem(DB_NAME)) || [];
+      // Filter out the user to be deleted
       const new_user_db = user_db.filter((user) => user.id !== id);
+      // Saving the updated user details in local storage
       localStorage.setItem(DB_NAME, JSON.stringify(new_user_db));
+      // Fetching and display the updated user details
       fetchUserDetails();
     }
   });
 }
-
+// Call the fetchUserDetails
 fetchUserDetails();
